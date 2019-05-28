a36e20d7bdba10c78d8369d1d6082a5ff99220e3
Lattapol Sae-Aue <mukaset@gmail.com>
2019-05-28 16:09:51 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Lattapol on 28/5/2562 BE.
+//  Copyright © 2562 Lattapol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
